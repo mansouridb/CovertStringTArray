ca4e45f878d8e80f9f0c3f667cc60320b0c4ff63
Majid <Mansouridb@gmail.com>
2021-04-22 11:54:22 +0200

    Initial commit

---
+++ src/main/java/ConvertStrTAr.java
@@ -0,0 +1,10 @@
+public class ConvertStrTAr {
+    public static void main(String[] args) {
+        String str = "ThisShouldBeConverted";
+        char[] charr = str.toCharArray();
+        for (char  item : charr) {
+            System.out.println(item);
+        }
+
+    }
+}
